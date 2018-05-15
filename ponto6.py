33be4d7e52e7bb7e3e44a4b25d7af7a25eb9bfef
JoaoMFF <joao_mff@hotmail.com>
2018-05-15 17:46:14 +0100

    ponto 6 WIP

---
+++ ponto6.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import string 
+
+def delete_zone_forward():
+    dominio_to_delete = raw_input("Insira  o dominio a eliminar: ")
+
+    zone_forward = '\nzone '+dominio_to_delete+' IN { \n	type master;\n	file "/var/named/'+dominio_input+'.hosts";\n};'
+
+    file = open("/etc/named.conf").read()
+    with open("/etc/named.conf", "a") as myfile:
+        for line in file:
+            myfile.write(line.replace(zone_forward, ''))
+        else:
+            print("Dominio não encontrado")
+            delete_zone_forward()
+
+    os.system("rm -f /var/named/"+dominio_to_delete+".hosts")
+
+def input_switch():
+
+    selection = input("Insert an option: \n1 - Delete Zone Forward\n2 - Delete Zone Reverse\n3 - Delete VirtualHosts\n")
+    
+    if selection == '1':
+        delete_zone_forward()
+    elif selection == '2':
+        print("Opção a")
+    elif selection == '3':
+        print("Opção b")
+    else:
+        print("Input inválido")
+        input_switch()
+
+if __name__ == '__main__':
+
+	input_switch()
